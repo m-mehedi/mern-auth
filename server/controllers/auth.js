aced4971b78e85f8a4b0f20523f848814b102e0a
Mehedi Hasan <me.mehedi@hotmail.com>
2022-02-18 03:05:02 +0600

    Signup user

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,32 @@
+const User = require('../models/user');
+
+
 exports.signup = (req, res) => {
-    console.log('REQ BODY ON SIGNUP',req.body);
+    // console.log('REQ BODY ON SIGNUP',req.body);
+    //     res.json({
+    //         data: 'You hit signup endpoint from Controller'
+    //     });
+    const {name, email, password } = req.body
+
+    User.findOne({email}).exec((err, user) => {
+        if(user){
+            return res.status(400).json({
+                error: 'Email is taken.'
+            })
+        }
+    })
+
+    let newUser = new User({name, email, password})
+
+    newUser.save((err, success) =>{
+        if(err){
+            console.log('SIGNUP ERROR', err)
+            return res.status(400).json({
+                error: err
+            });
+        }
         res.json({
-            data: 'You hit signup endpoint from Controller'
+            message: 'Signup success! Please signin'
         });
+    })
 }
