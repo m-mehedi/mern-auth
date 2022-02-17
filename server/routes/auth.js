666d46e8350c2352f184b93e701b12d8124855dd
Mehedi Hasan <me.mehedi@hotmail.com>
2022-02-18 02:54:18 +0600

    validate on signup

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,6 +4,11 @@ const router = express.Router();
 // import controller
 const {signup} = require('../controllers/auth');
 
-router.get('/signup', signup);
+// import validators
+const {userSignupValidator} = require('../validators/auth');
+const {runValidation} = require('../validators');
+
+router.post('/signup',userSignupValidator,runValidation, signup);
+// router.get('/signup', signup);
 
 module.exports = router
